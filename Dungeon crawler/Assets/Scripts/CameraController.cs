538d8f334d0fa6d73a0cf924bd21d9c8ff2340ff
SonilPro <51959110+SonilPro@users.noreply.github.com>
2020-01-24 19:38:35 +0100

    Edit

--- Dungeon crawler/Assets/Scripts/CameraController.cs
+++ Dungeon crawler/Assets/Scripts/CameraController.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+    [SerializeField]
+    private GameObject player;
+
+    private Vector3 offset;
+
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+
+    }
 
     void FixedUpdate()
     {
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + offset;
+
     }
 
 }
