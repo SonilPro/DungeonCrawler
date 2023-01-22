475f1340ce1c0c715b1e9a7227bd82a29fab0001
Antonio Ereiz <antonio.ereiz@gmail.com>
2023-01-22 12:49:38 +0100

    Enemy spawning types

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SpawnType
+{
+    Circle,
+    Rectangle
+}
+
 public class EnemySpawner : MonoBehaviour
 {
 
     [SerializeField] private GameObject[] enemies;
-    [SerializeField] private float spawnRange;
+    [SerializeField] private SpawnType spawnType = SpawnType.Circle;
+    [SerializeField] private float spawnRadius;
+    [SerializeField] private Vector2 spawnRange;
 
     private bool allDead = false;
     private bool spawnerEnabled = false;
@@ -37,7 +45,14 @@ public class EnemySpawner : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(transform.position, spawnRange);
+        if (spawnType == SpawnType.Circle)
+        {
+            Gizmos.DrawWireSphere(transform.position, spawnRadius);
+        }
+        else
+        {
+            Gizmos.DrawWireCube(transform.position, new Vector3(spawnRange.x, spawnRange.y, 0));
+        }
     }
 
     void Spawn()
@@ -46,7 +61,7 @@ public class EnemySpawner : MonoBehaviour
         EventHandler.StartDoorEvent(transform.parent.GetInstanceID());
         while (enemyCount-- > 0)
         {
-            Instantiate(enemies[Random.Range(0, enemies.Length)], RandomCircle(), transform.rotation, transform);
+            Instantiate(enemies[Random.Range(0, enemies.Length)], (spawnType == SpawnType.Circle) ? RandomCircle() : RandomRectangle(), transform.rotation, transform);
         }
     }
 
@@ -55,8 +70,20 @@ public class EnemySpawner : MonoBehaviour
         Vector3 center = transform.position;
         float ang = Random.value * 360;
         Vector3 pos;
-        pos.x = center.x + spawnRange * Mathf.Sin(ang * Mathf.Deg2Rad);
-        pos.y = center.y + spawnRange * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.x = center.x + spawnRadius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + spawnRadius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
+    }
+
+    private Vector3 RandomRectangle()
+    {
+        Vector3 center = transform.position;
+        Vector3 pos;
+        float halfWidth = spawnRange.x / 2;
+        float halfHeight = spawnRange.y / 2;
+        pos.x = Random.Range(center.x - halfWidth, center.x + halfWidth);
+        pos.y = Random.Range(center.y - halfHeight, center.y + halfHeight);
         pos.z = center.z;
         return pos;
     }
